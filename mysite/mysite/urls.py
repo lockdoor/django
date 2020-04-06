9e4ba098a342cc5e9049dcfac864adab4bcb8c1f
lockdoor <lockdoor@gmail.com>
2020-04-06 16:34:38 +0700

    aaaa

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index),
     path('polls/', include('polls.urls')),
     path('blogs/', include('blogs.urls')),
     ]
